24e489ba816db8ae779a14917cde35746e1f46d0
dhanushboga <dhanushboga555@gmail.com>
2025-06-14 22:08:14 +0530

    sg

---
+++ sg.tf
@@ -0,0 +1,8 @@
+module "sql_sg" {
+  source = "../sg-module"
+  Project_name = var.Project_name
+  environment = var.environment
+  common_tags = var.common_tags
+  sg_name = "mysql"
+  vpc_id = 
+}
